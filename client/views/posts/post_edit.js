744e7aad368aee9a0c01e2d8f3685e829c329b57
Rhys Powell <rhys@rpowell.me>
2013-05-23 12:03:00 +1000

    Added basic permission to check the post's owner

---
+++ client/views/posts/post_edit.js
@@ -0,0 +1,36 @@
+Template.postEdit.helpers({
+	post: function() {
+		return Posts.findOne(Session.get('currentPostId'));
+	}
+});
+
+Template.postEdit.events({
+	'submit form': function(e) {
+		e.preventDefault();
+
+		var currentPostId = Session.get('currentPostId');
+
+		var postProperties = {
+			url: $(event.target).find('[name=url]').val(),
+			title: $(event.target).find('[name=title]').val()
+		};
+
+		Posts.update(currentPostId, {$set: postProperties}, function(error) {
+			if (error) {
+				alert(error.reason);
+			} else {
+				Meteor.Router.to('postPage', currentPostId);
+			}
+		})
+	},
+
+	'click .delete': function(e) {
+		e.preventDefault();
+
+		if (confirm("Delete this post?")) {
+			var currentPostId = Session.get('currentPostId');
+			Posts.remove(currentPostId);
+			Meteor.Router.to('postsList');
+		}
+	}
+});
